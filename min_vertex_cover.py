2b8bf534d77d3696042c321880de2736e91145e5
Felix Ullmann <ullmann206@gmail.com>
2022-12-20 09:31:28 -0600

    initial commit  - add exact solver for (unweighted) min vertex cover

---
+++ min_vertex_cover.py
@@ -0,0 +1,56 @@
+from gurobipy import *
+
+import sys
+import time
+import networkx as nx
+
+sys.stdout.write("#")
+
+G = nx.Graph()
+max_clique_size = 0
+
+for line in open(sys.argv[1]):
+    fields = line.split()
+    u = fields[0]
+    v = fields[1]
+    G.add_node(u)
+    G.add_node(v)
+    G.add_edge(u, v)
+
+print("G has", G.number_of_nodes(), "vertices and",
+      G.number_of_edges(), "edges.", file=sys.stderr)
+
+
+start = time.time()
+
+try:
+
+    # Model with variables
+    m = Model("Minimum Vertex Cover")
+    for v in G.nodes:
+        m.addVar(vtype=GRB.BINARY, name=v)
+    m.update()
+
+    # Objective Function#
+    obj = LinExpr()
+    for v in G.nodes:
+        obj.add(m.getVarByName(v))
+    m.setObjective(obj, GRB.MINIMIZE)
+    m.update()
+
+    for (u, v) in G.edges:
+        xu = m.getVarByName(u)
+        xv = m.getVarByName(v)
+        m.addConstr(xu + xv >= 1)
+
+    m.update()
+    m.optimize()
+
+    print("Optimized.\nTotal running time:", "{:7.3f}".format(
+        time.time()-start), "s.\nCurrent max clique size:", max_clique_size)
+
+    # TODO: Add output
+
+
+except GurobiError as e:
+    print(e.message)

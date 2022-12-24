25e1a1924ed8faddda9216e547415662649698a1
Felix Ullmann <ullmann206@gmail.com>
2022-12-24 09:03:56 -0600

    fix check_solution.py

--- check_solution.py
+++ check_solution.py
@@ -2,8 +2,8 @@ import sys
 import os
 import networkx as nx
 
-SOLUTION_FILE = sys.argv[1]
-FOLDER_PATH = os.path.dirname(SOLUTION_FILE)
+FOLDER_PATH = sys.argv[1]
+print()
 
 # Read graph
 G = nx.Graph()
@@ -19,21 +19,42 @@ with open(FOLDER_PATH + '/conflict_graph.txt') as file:
         u, v = line.split()
         G.add_edge(u, v)
 
-print(f"Vertices: {len(G.nodes)}")
-print(f"Edges: {len(G.edges)}")
-
-solution = set()
-
-with open(SOLUTION_FILE) as file:
+complement_derived_vertex_cover = set()
+with open(FOLDER_PATH + '/lploads.txt') as file:
     for line in file:
-        solution.add(line.rstrip('\n'))
+        node, included = line.split()
+        if int(float(included)) == 0:
+            complement_derived_vertex_cover.add(node)
 
-leftover = G.copy()
+calculated_vertex_cover = set()
+with open(FOLDER_PATH + '/_min_weighted_vertex_cover.sol') as file:
+    for line in file:
+        line = line.rstrip('\n')
+        calculated_vertex_cover.add(line)
+        if not G.has_node(line):
+            print(f"Solution contains non-existing vertex {line}")
+            sys.exit(1)
 
+
+complement_weight = 0
+calculated_weight = 0
 
 for vertex in G.nodes:
-    if vertex in solution:
-        leftover.remove_node(vertex)
+    if vertex in complement_derived_vertex_cover:
+        complement_weight += G.nodes[vertex]['weight']
+    if vertex in calculated_vertex_cover:
+        calculated_weight += G.nodes[vertex]['weight']
 
 
-print(f"Edges: {len(leftover.edges)}")
+print(
+    f'Complement-derived vertex cover node count: {len(complement_derived_vertex_cover)}')
+print(f'Complement-derived vertex cover weight: {complement_weight}')
+print()
+print(f'Calcualted vertex cover node count: {len(calculated_vertex_cover)}')
+print(f'Calculated vertex cover weight: {calculated_weight}')
+
+print()
+if (complement_derived_vertex_cover == calculated_vertex_cover):
+    print("Vertex covers are equal.")
+else:
+    print("Vertex covers are not equal.")
